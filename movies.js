64fde07bd3ba17d06ced2d5364d57683cf6ba9f9
KEVIN LAMARCA <LAMARCA.K@GMAIL.COM>
2022-01-29 17:56:30 -0500

    lab 10 features added

--- movies.js
+++ movies.js
@@ -1,29 +1,37 @@
 'use strict';
 
+const { response } = require('express');
 const axios = require('axios')
+const cache = require('./cache.js');
 
 async function getMovies(request, response) {
   let location = request.query.location;
   let movieDbUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${location}&include_adult=false`;
-  try {
+  let key = `movie${location}`
+  if (cache[key] && Date.now() - cache[key].timestamp < 50000) {
+    console.log('cache hit');
+  } else {
+    console.log('cache miss')
     let movieData = await axios.get(movieDbUrl);
     movieData = movieData.data.results; // movieData is assigned an array of objs
-    let groomedMovieData = movieData.map(obj => new Movie(obj));
-    response.send(groomedMovieData);
-  } catch {
-    response.send('Could not retrieve movie data from API')
-  }
-};
-
-class Movie {
-  constructor(obj) {
-    this.title = obj.title
-    this.overview = obj.overview
-    this.average_votes = obj.vote_average
-    this.image_url = `https://image.tmdb.org/t/p/w500${obj.backdrop_path}`
-    this.popularity = obj.popularity
-    this.released_on = obj.release_date
+    cache[key] = {
+      timestamp: Date.now(),
+      data: movieData
+    };
   };
-};
+  let groomedMovieData = cache[key].data.map(obj => new Movie(obj));
+  response.send(groomedMovieData);
+}
 
-module.exports = getMovies;
+  class Movie {
+    constructor(obj) {
+      this.title = obj.title
+      this.overview = obj.overview
+      this.average_votes = obj.vote_average
+      this.image_url = `https://image.tmdb.org/t/p/w500${obj.backdrop_path}`
+      this.popularity = obj.popularity
+      this.released_on = obj.release_date
+    };
+  };
+
+  module.exports = getMovies;
--- weather.js
+++ weather.js
@@ -1,36 +1,35 @@
 'use strict';
 
-let cache = require('./cache.js');
+const { response } = require('express');
+const cache = require('./cache.js');
+const axios = require('axios');
 
-module.exports = getWeather;
-
-function getWeather(latitude, longitude) {
+async function getWeather (request, response) {
+  let latitude = request.query.lat;
+  let longitude = request.query.lon;
   const key = 'weather-' + latitude + longitude;
-  const url = `http://api.weatherbit.io/v2.0/forecast/daily/?key=${WEATHER_API_KEY}&lang=en&lat=${lat}&lon=${lon}&days=5`;
-
+  const url = `http://api.weatherbit.io/v2.0/forecast/daily/?key=${process.env.WEATHER_API_KEY}&lang=en&lat=${latitude}&lon=${longitude}&days=5`;
+  
   if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
     console.log('Cache hit');
+
   } else {
     console.log('Cache miss');
     cache[key] = {
-      timestamp = Date.now(),
-      data = axios.get(url)
+      timestamp: Date.now(),
+      data: await axios.get(url)
     };
-    .then(response => parseWeather(response.body));
   }
-  
-  return cache[key].data;
+  let parsedWeather = parseWeather(cache[key].data);
+  response.send(parsedWeather)
 }
 
 function parseWeather(weatherData) {
-  try {
-    const weatherSummaries = weatherData.data.map(day => {
+    console.log(weatherData)
+    const weatherSummaries = weatherData.data.data.map(day => {
       return new Weather(day);
     });
-    return Promise.resolve(weatherSummaries);
-  } catch (e) {
-    return Promise.reject(e);
-  }
+    return weatherSummaries;
 }
 
 class Weather {
@@ -39,3 +38,5 @@ class Weather {
     this.time = day.datetime;
   }
 }
+
+module.exports = getWeather;
